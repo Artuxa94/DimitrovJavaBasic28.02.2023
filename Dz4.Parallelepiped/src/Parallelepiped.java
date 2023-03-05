bd7fd85eb54a9aba05d2c05be42371e1c4df2e91
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-05 12:05:04 +0100

    addVolume

--- Dz4.Parallelepiped/src/Parallelepiped.java
+++ Dz4.Parallelepiped/src/Parallelepiped.java
@@ -1,5 +1,9 @@
 public class Parallelepiped {
     public static void main (String [] args) {
-        
+        int length = 4;
+        int width = 5;
+        int height = 6;
+        int volume = length*width*height;
+        System.out.println("Обьем параллелипипеда: "+volume);
     }
 }
