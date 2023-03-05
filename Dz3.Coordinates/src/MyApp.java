a20b8fac28a12a54c8262c8166cea62cc249fd81
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-05 11:41:06 +0100

    addCoordinates

--- Dz3.Coordinates/src/MyApp.java
+++ Dz3.Coordinates/src/MyApp.java
@@ -1,5 +1,9 @@
+import java.math.BigDecimal;
+
 public class MyApp {
     public static void main (String[] args) {
-        
+        double latitude = 46.77104965139757;
+        double longitude = 36.79531095986597;
+        System.out.println("My home coordinates: Latitude " + latitude + ", Longitude "+ longitude);
     }
 }
