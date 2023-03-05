dcd7b1db67a9a0f0039c5add1f73d509f2e4c515
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-05 12:51:31 +0100

    addPower

--- Dz5.ChineseDynasties/src/ChineseDynasties.java
+++ Dz5.ChineseDynasties/src/ChineseDynasties.java
@@ -1,5 +1,13 @@
 public class ChineseDynasties {
     public static void main(String[] args) {
-        
+        int liWar = 13*860;
+        int liArcher = 24*860;
+        int liRider = 46*860;
+        int liAttack = liWar+liArcher+liRider;
+        double minWar = 9*860*1.5;
+        double minArcher = 35*860*1.5;
+        double minRider = 12*860*1.5;
+        double minAttack = minWar+minArcher+minRider;
+        System.out.println("Power Li: " + liAttack + ", Power Min: " + minAttack);
     }
 }
