e94c176ebb828d44764a02abfc48b3c5f4479e5c
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-07 18:32:10 +0100

    addLength

--- Dz4.Parallelepiped/src/Parallelepiped.java
+++ Dz4.Parallelepiped/src/Parallelepiped.java
@@ -1,9 +1,10 @@
 public class Parallelepiped {
     public static void main (String [] args) {
-        int length = 4;
-        int width = 5;
-        int height = 6;
-        int volume = length*width*height;
-        System.out.println("Обьем параллелипипеда: "+volume);
+        int sideLength = 4;
+        int sideWidth = 5;
+        int sideHeight = 6;
+        int volume = sideLength*sideWidth*sideHeight;
+        int length = sideLength+sideWidth+sideHeight;
+        System.out.println("Обьем параллелипипеда: " + volume + ", Сумарная длина всех сторон: " + length);
     }
 }
