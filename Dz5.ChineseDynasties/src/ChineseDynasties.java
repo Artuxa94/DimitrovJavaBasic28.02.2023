693966b5e0d6a98e6305625000342ab0b6d5ce57
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-16 20:42:10 +0100

    addVariablesCycle

--- Dz5.ChineseDynasties/src/ChineseDynasties.java
+++ Dz5.ChineseDynasties/src/ChineseDynasties.java
@@ -1,13 +1,20 @@
 public class ChineseDynasties {
     public static void main(String[] args) {
-        int liWar = 13*860;
-        int liArcher = 24*860;
-        int liRider = 46*860;
+        int quantityUnit = 860;
+        double multiplier = 1.5;
+        int liWar = 13*quantityUnit;
+        int liArcher = 24*quantityUnit;
+        int liRider = 46*quantityUnit;
         int liAttack = liWar+liArcher+liRider;
-        double minWar = 9*860*1.5;
-        double minArcher = 35*860*1.5;
-        double minRider = 12*860*1.5;
+        double minWar = 9*quantityUnit*multiplier;
+        double minArcher = 35*quantityUnit*multiplier;
+        double minRider = 12*quantityUnit*multiplier;
         double minAttack = minWar+minArcher+minRider;
+        if (liAttack > minAttack) {
+            System.out.println("Lee Win");
+        } else {
+            System.out.println("Min Win");
+        }
         System.out.println("Power Li: " + liAttack + ", Power Min: " + minAttack);
     }
 }
