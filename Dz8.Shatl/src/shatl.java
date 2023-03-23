bc9bafe8a919f312e3b8da312bab8701ac739dc8
Artuxa94 <arturdimitrov1994@gmail.com>
2023-03-23 21:55:31 +0100

    rewrote the function

--- Dz8.Shatl/src/shatl.java
+++ Dz8.Shatl/src/shatl.java
@@ -1,5 +1,17 @@
 public class shatl {
     public static void main(String[] args) {
-
+        int shatlsName = 0;
+        int index = 0;
+        while (true) {
+            shatlsName++;
+            if (String.valueOf(shatlsName).contains("4") || String.valueOf(shatlsName).contains("9")){
+                continue;
+            }
+            index++;
+            System.out.println(shatlsName + " номер шатла");
+            if (index == 100){
+                break;
+            }
+        }
     }
 }
