0b07a0872c2723fb6618c664cb49330c85fa14d9
Artuxa94 <arturdimitrov1994@gmail.com>
2023-04-01 15:54:15 +0200

    done

--- Dz12.PersonInfo/src/Main.java
+++ Dz12.PersonInfo/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+    Person person1 = new Person("Artur","Dimitrov","Berdyansk","5435435352");
+    Person person2 = new Person("Alexandra","Dodarrio","Los Angeles","432442423");
+    Person person3 = new Person("Emily","Ratajkowski", "New Yourk", "435657468765");
+        System.out.println(person1.personInfo());
+        System.out.println(person2.personInfo());
+        System.out.println(person3.personInfo());
 
     }
 }
--- Dz12.PersonInfo/src/Person.java
+++ Dz12.PersonInfo/src/Person.java
@@ -1,2 +1,18 @@
 public class Person {
+    public String name;
+    public String surName;
+    public String city;
+    public String phoneNumb;
+
+    public Person (String name, String surName, String city, String phoneNumb) {
+        this.name = name;
+        this.surName = surName;
+        this.city = city;
+        this.phoneNumb = phoneNumb;
+    }
+
+    public String personInfo() {
+        return "Зателефонувати громадянину " + name + " " + surName + " із міста " + city + " можна за номером - " + phoneNumb;
+    }
 }
+
