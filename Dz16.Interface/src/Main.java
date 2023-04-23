d3568480764a19f5a70bbb7db60660ec4a2051ac
Artuxa94 <arturdimitrov1994@gmail.com>
2023-04-24 00:26:09 +0200

    init

---
+++ Dz16.Interface/src/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    iPhones iphone = new iPhones();
+
+    iphone.call(75768);
+    iphone.sms(78987,"Hi iPhone");
+    iphone.internet();
+    iphone.info();
+    iphone.appStore();
+
+    System.out.println("---------------------------------");
+    Androids android = new Androids();
+
+    android.call(54353453);
+    android.sms(53453453,"Hi Androids");
+    android.internet();
+    android.info();
+    android.googlePlay();
+
+
+    }
+}
