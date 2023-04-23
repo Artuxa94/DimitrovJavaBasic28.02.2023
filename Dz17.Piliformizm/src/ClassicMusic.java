0bcf32c10ad91bfeaf3090627aa47c165c4e4e71
Artuxa94 <arturdimitrov1994@gmail.com>
2023-04-24 00:27:23 +0200

    allDone

---
+++ Dz17.Piliformizm/src/ClassicMusic.java
@@ -0,0 +1,10 @@
+public class ClassicMusic extends MusicStyles{
+    public ClassicMusic(String name, String type) {
+        super(name, type);
+    }
+
+    @Override
+    public void playMusic() {
+        System.out.println("Тудудун Тудудун");
+    }
+}
