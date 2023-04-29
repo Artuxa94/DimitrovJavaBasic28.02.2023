d0f4fddea147ebe8ca46d47b46058c426389ae1c
Artuxa94 <arturdimitrov1994@gmail.com>
2023-04-29 19:28:35 +0200

    addTestArrMethode

--- Dz17.Test/src/test/java/org/example/TestArrMethod.java
+++ Dz17.Test/src/test/java/org/example/TestArrMethod.java
@@ -1,6 +1,25 @@
 package org.example;
 
 
-public class TestArrMethod {
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
 
+public class TestArrMethod {
+    @Test
+    public void testAverege() {
+        int[] arr = {5,5,5};
+        double expected = 5.0;
+        double actual = ArrMethod.averege(arr);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void testTrueSquare () {
+        int[][] arr = new int[2][2];
+        Assert.assertTrue(ArrMethod.itSquare(arr));
+    }
+    @Test
+    public void testFalseSquare() {
+        int[][] arr = new int[2][3];
+        Assert.assertFalse(ArrMethod.itSquare(arr));
+    }
 }
